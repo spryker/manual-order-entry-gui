2a012f6d11127cd28c81eb84c70b2014405a7c72
Ahmed Sabaa <ahmed.sabaa@spryker.com>
2022-02-09 11:38:01 +0100

    squashed single decouple commit

--- src/Spryker/Zed/ManualOrderEntryGui/Communication/Form/Payment/PaymentType.php
+++ src/Spryker/Zed/ManualOrderEntryGui/Communication/Form/Payment/PaymentType.php
@@ -118,7 +118,7 @@ class PaymentType extends AbstractType
         foreach ($paymentMethodSubForms as $paymentMethodSubForm) {
             $builder->add(
                 $paymentMethodSubForm[static::KEY_PLUGIN]->getName(),
-                get_class($paymentMethodSubForm[static::KEY_SUBFORM]) ?: null,
+                get_class($paymentMethodSubForm[static::KEY_SUBFORM]),
                 [
                     'property_path' => static::PAYMENT_PROPERTY_PATH . '.' . $paymentMethodSubForm[static::KEY_PLUGIN]->getPropertyPath(),
                     'error_bubbling' => true,
